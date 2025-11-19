eb9dc9d484ea23291451baa010c96c93b75d5ec0
mavapa369 <mavapa369@yandex.ru>
2025-11-20 00:55:16 +0500

    #7 Feauture: Category Model corrections 1

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,8 +5,10 @@ All models are imported here for Alembic autogenerate to work.
 """
 from app.db.session import Base
 from app.models.user import User
+from app.models.category import Category
 
 __all__ = [
     "Base",
     "User",
+    "Category",
 ]
--- app/models/category.py
+++ app/models/category.py
@@ -1,18 +1,10 @@
-from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey, UniqueConstraint, Index
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, UniqueConstraint, Index
 from sqlalchemy.orm import relationship
 
-# from .user import Base, User
 from app.db.session import Base
 
-
-# id, name, description (optional), user_id (FK), created_at, updated_at
 class Category(Base):
-    __tablename__ = 'category'
-    id = Column(
-        Integer, 
-        primary_key=True
-        )
+    __tablename__ = 'categories'
     name = Column(
         String(100), 
         nullable=False,
@@ -29,11 +21,10 @@ class Category(Base):
         nullable=False,
         comment="User who owns this category"
     )
-
     __table_args__ = (
         UniqueConstraint('name', 'user_id', name='uq_user_category_name'),
         Index('ix_category_user_id', 'user_id'),
     )
 
     user = relationship("User", back_populates="categories")
-    task = relationship("Task", back_populates="categories")
+    # task = relationship("Task", back_populates="category")
