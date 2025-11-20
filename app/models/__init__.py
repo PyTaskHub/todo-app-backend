5f45918c7a6dcbbf56b8dd2fa8d8cbf90a1f01c1
Elraden <morozovsa94@yandex.ru>
2025-11-20 20:35:58 +0300

    #6 Feature: Task Model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,10 +5,13 @@ All models are imported here for Alembic autogenerate to work.
 """
 from app.db.session import Base
 from app.models.user import User
+
 from app.models.category import Category
+from app.models.task import Task
 
 __all__ = [
     "Base",
     "User",
     "Category",
+    "Task",
 ]
